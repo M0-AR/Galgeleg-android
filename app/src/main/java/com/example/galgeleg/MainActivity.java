496afd5c56a59504ab33ae3dd034fd3255302bf8
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-10-11 19:43:53 +0200

    Hello World

---
+++ app/src/main/java/com/example/galgeleg/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.galgeleg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Galgelogik spil = new Galgelogik();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
