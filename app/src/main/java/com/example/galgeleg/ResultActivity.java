aecc5b33355f0ccada17fc654063e89d03ea2121
Mohamad-Ashmar <s176492@student.dtu.dk>
2020-10-22 22:27:11 +0200

    Delete save and load code to and from txt file, because I need to store every object with its attributes, which serialize an object is a solution for this.

--- app/src/main/java/com/example/galgeleg/ResultActivity.java
+++ app/src/main/java/com/example/galgeleg/ResultActivity.java
@@ -2,9 +2,12 @@ package com.example.galgeleg;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -12,9 +15,21 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+// https://stackoverflow.com/questions/4118751/how-do-i-serialize-an-object-and-save-it-to-a-file-in-android
+// https://stackoverflow.com/questions/27409718/java-reading-multiple-objects-from-a-file-as-they-were-in-an-array
+// https://stackoverflow.com/questions/31339075/getting-multiple-objects-from-serialization-in-java
 
 public class ResultActivity extends Activity {
     public static final String FILE_NAME = "result.txt";
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private ArrayList<ResultItem> mResultItemList = new ArrayList<>();
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -35,31 +50,32 @@ public class ResultActivity extends Activity {
         TextView textView3 = findViewById(R.id.textView3);
         textView3.setText( wrongLetters);
 
-        save(text + "\n" + correctLetters + "\n" + wrongLetters);
+        save(text, correctLetters, wrongLetters);
     }
 
 
     public void load() {
-        try(FileInputStream fis = openFileInput(FILE_NAME)) {
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader br = new BufferedReader(isr);
-            StringBuilder sb = new StringBuilder();
-            String text;
-            while ((text = br.readLine()) != null)
-                sb.append(text).append("\n");
-        } catch (IOException e) {
+        try(FileInputStream fis = openFileInput(FILE_NAME);
+            ObjectInputStream is = new ObjectInputStream(fis)) {
+            while (true) {
+                ResultItem resultItem = (ResultItem) is.readObject();
+                if (resultItem != null)
+                    mResultItemList.add(resultItem);
+                else
+                    break;
+            }
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public void save(String s) {
-        try (FileOutputStream fos = openFileOutput(FILE_NAME, MODE_PRIVATE)) {
-            fos.write(s.getBytes());
+    public void save(String resultText, String correctLetters, String wrongLetters) {
+        try (FileOutputStream fos = openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+             ObjectOutputStream os = new ObjectOutputStream(fos)) {
+            os.writeObject(new ResultItem(resultText, correctLetters, wrongLetters));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
 }
 
